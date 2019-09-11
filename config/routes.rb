9c27f3d8f9c4bc2bc7f9d692a433aa6083bd5a53
SmulskyiIhor <smulia755@gmail.com>
2019-09-11 16:21:37 +0300

    add registrations_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespase :api, defaults: {format: :json} do
+    namespase :v1 do
+      post "sign_up", to: "registrations#create"
 end
