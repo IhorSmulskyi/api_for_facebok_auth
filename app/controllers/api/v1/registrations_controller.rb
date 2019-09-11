fd03f669ccc4a4c3b30739250531b44ecb7496cb
SmulskyiIhor <smulia755@gmail.com>
2019-09-11 17:47:24 +0300

    fix mistake

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
     
     private
     def user_params
-        params.require(:user).permit(:email, :password, :pasword_confirmation)
+        params.require(:user).permit(:email, :password, :password_confirmation)
 		end
 		
 		def ensure_params_exist
