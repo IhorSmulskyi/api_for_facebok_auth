fe3c47815cdf240f0c06f090b4ae9f25417cc411
SmulskyiIhor <smulia755@gmail.com>
2019-09-12 11:15:33 +0300

    add gem koala and facebook app_id

---
+++ config/initializers/koala.rb
@@ -0,0 +1,6 @@
+Koala.configure do |config|
+    config.app_id = "471280757047696"
+    config.app_secret = "58a8be5228ce0974133f1a21687e1ba7"
+    # See Koala::Configuration for more options, including details on how to send requests through
+    # your own proxy servers.
+  end
