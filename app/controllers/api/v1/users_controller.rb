cbb693a6426d94a277ddffded10141f151b7de9e
SmulskyiIhor <smulia755@gmail.com>
2019-09-19 12:56:34 +0300

    add rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::UsersController < ApplicationController
   before_action :choose_provider
   def choose_provider
@@ -6,36 +8,34 @@ class Api::V1::UsersController < ApplicationController
     # http.use_ssl = true
     # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    if uri.host == "graph.facebook.com"
-      @provider = "facebook"
+    if uri.host == 'graph.facebook.com'
+      @provider = 'facebook'
       @getdata = ::GetFacebookData.new.get_facebook_data(uri).body
-    if uri.host == "twitter"
-      @provider = "twitter"
-      @getdata = @getdata = ::GetTwitterData.new.get_twitter_data(uri).body
-    else 
-      @provider = "unnown"
-    end
+      if uri.host == 'twitter'
+        @provider = 'twitter'
+        @getdata = @getdata = ::GetTwitterData.new.get_twitter_data(uri).body
+      else
+        @provider = 'unnown'
+      end
     end
   end
 
   def facebook
     user_data = JSON.parse(@getdata)
-    user = User.find_by email: user_data["email"]
+    user = User.find_by email: user_data['email']
     binding.pry
     if user
       user.generate_new_authentication_token
-      json_response "User Information", true, {user: user}, :ok
+      json_response 'User Information', true, { user: user }, :ok
     else
       user = ::RegistrationUseProvider.new(user_data, @provider)
       user.authentication_token = User.generate_unique_secure_token
 
       if user.save
-        json_response "Login Facebook Successfully", true, {user: user}, :ok
+        json_response 'Login Facebook Successfully', true, { user: user }, :ok
       else
         json_response user.errors, false, {}, :unprocessable_entity
       end
     end
-    
   end
-
 end
--- app/services/get_facebook_data.rb
+++ app/services/get_facebook_data.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'net/https'
 
-
 class GetFacebookData
- 
-  
-
   def get_facebook_data(uri)
-    @token = "EAAGsoJodAZAABAMoobX5Y70TahMNlUEPIaw8jFg27kWX1ZBRhSQAF7NLIYcQRlNRHwwsceDLyaOBDptPr4pAKflfOyTSGZCmIn3QakZAzZCFeRXZBDC4FK6YnIpNcZBl5VrTZB0Ldt1HZA4FI55efELXSV9pvJlGvhNZB2zdAf1Rry4bxUDeqtVa9rqiZC4gsRmIDeQ6WzABLpiqQZDZD"
+    @token = 'EAAGsoJodAZAABAMoobX5Y70TahMNlUEPIaw8jFg27kWX1ZBRhSQAF7NLIYcQRlNRHwwsceDLyaOBDptPr4pAKflfOyTSGZCmIn3QakZAzZCFeRXZBDC4FK6YnIpNcZBl5VrTZB0Ldt1HZA4FI55efELXSV9pvJlGvhNZB2zdAf1Rry4bxUDeqtVa9rqiZC4gsRmIDeQ6WzABLpiqQZDZD'
 
     # uri = URI.parse(ENV['URL_BASE'])
     http = Net::HTTP.new(uri.host, uri.port)
